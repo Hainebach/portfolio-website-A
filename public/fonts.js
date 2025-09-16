9f5261362f087e409f08cbc0b6264c8f46d1f195
Hainebach <117458965+Hainebach@users.noreply.github.com>
2025-09-16 14:25:08 +0200

    clean old font file

--- public/fonts.js
+++
@@ -1,9 +0,0 @@
-import { PT_Serif } from "next/font/google";
-
-// Configure PT Serif font
-export const ptSerif = PT_Serif({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-pt-serif",
-});
