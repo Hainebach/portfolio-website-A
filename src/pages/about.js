c4aa557554cb7896b89d1f04ffcc37ece2576f50
Hainebach <117458965+Hainebach@users.noreply.github.com>
2026-03-23 15:58:27 +0100

    change dynamic behavior or logoz

--- src/pages/about.js
+++ src/pages/about.js
@@ -40,7 +40,7 @@ export default function About({ info }) {
   };
 
   const ClientLogosGrid = ({ logos }) => (
-    <div className="grid grid-cols-3 md:grid-cols-4 w-full gap-8 mt-16">
+    <div className="grid grid-cols-3 lg:grid-cols-4 w-full gap-8 mt-16">
       {logos?.map((logo, index) => (
         <div key={index} className="relative h-20 md:h-28 w-20 md:w-28">
           <Image
